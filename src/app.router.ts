57980acab81b8b1eb5f7ea2c330e4001e290b7d2
Guille Cosano <i62cogag@uco.es>
2021-11-23 19:15:29 +0100

    feat: post new fetch exchange data by code every 5 min

--- src/app.router.ts
+++ src/app.router.ts
@@ -4,17 +4,32 @@ import cron from "node-cron";
 import { getCurrencyExchangeByCode } from "./currency/application";
 import { CurrencyAggregate } from "./currency/domain";
 import { CurrencyController } from "./currency";
+import CurrencyHistory from "./currency-history/infrastructure/schema/currency-history.schema";
 
 const appRouter: Router = Router();
 
 const currencyController = new CurrencyController();
 
-cron.schedule("*/1 * * * *", async () => {
+cron.schedule("*/5 * * * *", async () => {
   // ---- CRON GETTING FOLLOWED CURRENCIES EVERY 5 MINUTES
   const currencies = await currencyController.findAll();
   currencies.map((currency: CurrencyAggregate) => {
     getCurrencyExchangeByCode(currency.code)
-      .then((res) => console.debug(res.data))
+      .then((res) => {
+        const newCurrencyHistory = new CurrencyHistory({
+          code: res.data["Realtime Currency Exchange Rate"][
+            "1. From_Currency Code"
+          ],
+          values: {
+            bid: res.data["Realtime Currency Exchange Rate"]["8. Bid Price"],
+            ask: res.data["Realtime Currency Exchange Rate"]["9. Ask Price"],
+            date: res.data["Realtime Currency Exchange Rate"][
+              "6. Last Refreshed"
+            ],
+          },
+        });
+        newCurrencyHistory.save();
+      })
       .catch((e) => console.error(e));
   });
 });
