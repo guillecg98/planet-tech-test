d694b9ee3e0c177f7b216266d4859927e2be1067
Guille Cosano <i62cogag@uco.es>
2021-11-23 14:40:53 +0100

    chore: db configured

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,23 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+require("dotenv").config();
+
+async function connectToDatabase(connectionUri: string) {
+  return new Promise((resolve, reject) => {
+    // From mongoose@6.x.x onwards useNewUrlParser, useUnifiedTopology,
+    // useCreateIndex are deprecated and default to true
+    mongoose
+      .connect(connectionUri)
+      .then(() => {
+        console.log("Connected to DB");
+        resolve(connectionUri);
+      })
+      .catch((error: any) => {
+        console.log(error);
+        reject(`${connectionUri}: ${error}`);
+      });
+  });
+}
 
 const app = express();
 const port = 3000;
@@ -6,6 +25,10 @@ const port = 3000;
 app.get("/health", async (req, res) => {
   res.status(200).json({ healthy: true });
 });
+
+connectToDatabase(
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@127.0.0.1:27017/${process.env.DB_NAME}?authSource=admin`
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
